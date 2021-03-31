0406ee2c23094e5493d8f3973a6f6aeb7d9af5c0
mkalema <mkgremo20@gmail.com>
2021-03-30 23:39:47 -0500

    added swagger configurations for api documentation

--- src/main/java/com/karuna/pages/KarunaPagesApplication.java
+++ src/main/java/com/karuna/pages/KarunaPagesApplication.java
@@ -12,17 +12,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication()
-@EnableSwagger2
 public class KarunaPagesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KarunaPagesApplication.class, args);
-    }
-
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.karuna.pages")).build();
     }
 
 }
