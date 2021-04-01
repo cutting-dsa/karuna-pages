94ecd27027df26e3340a0b0f6b77c6cc9751aca9
mkalema <mkgremo20@gmail.com>
2021-03-31 22:41:58 -0500

    added base url and modified swagger configurations

--- src/main/java/com/karuna/pages/user/controller/UserController.java
+++ src/main/java/com/karuna/pages/user/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/user")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
